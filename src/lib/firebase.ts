108f4c170255d77492ff5d05a42a94c79a9ae807
Sharonmee <enamyawlui@gmail.com>
2025-06-06 22:09:17 +0000

    Refactor Firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GithubAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    "apiKey": "AIzaSyDs1FzXruAInN-Yx03SqD3cWQo8BVQbnww",
-    "authDomain": "myportfolio-249cf.firebaseapp.com",
-    "projectId": "myportfolio-249cf",
-    "storageBucket": "myportfolio-249cf.appspot.com",
-    "messagingSenderId": "991084925181",
-    "appId": "1:991084925181:web:23ed8656ca51f8350b6e94",
-    "measurementId": "G-1J0XC8XSCJ",
-    "PORTFOLIO_GENERATOR_URL": "https://4jxyzpdgu6.execute-api.eu-west-2.amazonaws.com/test/",
-    "BASE_URL": "https://enpo5jyn97.execute-api.eu-west-2.amazonaws.com/Test/",
-    "PROJECTS_BASE_URL": "https://h99pu2exbe.execute-api.eu-west-2.amazonaws.com/prod"
-
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
