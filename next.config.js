0a7d5a8788d3344b2d2e4268c94abea6cbbdda9f
Sharonmee <enamyawlui@gmail.com>
2025-06-06 20:39:43 +0000

    Update .gitignore to include amplify.yml and enhance next.config.js with standalone output, trailing slashes, and custom rewrites and redirects for improved routing.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,39 @@ const nextConfig = {
     images: {
         domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
     },
+    output: 'standalone',
+    trailingSlash: true,
+    async rewrites() {
+        return [
+            {
+                source: '/myportfolio',
+                destination: '/myportfolio/',
+            },
+            {
+                source: '/myportfolio/:username',
+                destination: '/myportfolio/:username/',
+            },
+        ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/index.html',
+                permanent: true,
+            },
+            {
+                source: '/myportfolio',
+                destination: '/myportfolio/',
+                permanent: true,
+            },
+            {
+                source: '/myportfolio/:username',
+                destination: '/myportfolio/:username/',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
