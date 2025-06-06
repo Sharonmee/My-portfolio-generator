e8ca489b443c802123b89f67923f8a68fbbbfd39
Sharonmee <enamyawlui@gmail.com>
2025-06-06 21:30:12 +0000

    fix again

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
     images: {
         domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
     },
     output: 'standalone',
-    trailingSlash: true,
     async rewrites() {
         return [
             {
